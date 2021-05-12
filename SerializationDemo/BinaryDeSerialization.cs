2d0f61c5a5d416a3638aba4dcdbd4e892db01bdb
victor-singha <vicunite2@gmail.com>
2021-05-12 13:05:24 +0530

    initial commit

---
+++ SerializationDemo/BinaryDeSerialization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationDemo
+{
+    class BinaryDeSerialization
+    {
+        public void DeSerialization()
+        {
+            FileStream fileStream = new FileStream(@"/Example.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Demo deserializationdemo = (Demo)formatter.Deserialize(fileStream);
+            Console.WriteLine($"Application name {deserializationdemo.ApplicationName} ---ApplicationId    {deserializationdemo.ApplicationId}");
+            Console.ReadKey();
+        }
+    }
+}
