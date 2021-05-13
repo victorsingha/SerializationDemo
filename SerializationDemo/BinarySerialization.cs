f2bbcd2d971ff03dd0b8385d8dc86a7de7858dcd
victor-singha <vicunite2@gmail.com>
2021-05-13 11:37:00 +0530

    [victor] change path to absolute

--- SerializationDemo/BinarySerialization.cs
+++ SerializationDemo/BinarySerialization.cs
@@ -14,7 +14,7 @@ namespace SerializationDemo
         public void Serialization()
         {
             Demo sample = new Demo();
-            FileStream fileStream = new FileStream(@"\Example.txt", FileMode.Create);
+            FileStream fileStream = new FileStream(@"C:\Users\vicun\source\repos\SerializationDemo\SerializationDemo\Example.txt", FileMode.Create);
             BinaryFormatter formatter = new BinaryFormatter();
             formatter.Serialize(fileStream, sample);
             Console.ReadKey();
